e4d6ba025c10461d44bbb079f60830ea9732aca1
jumagit <mukoovajuma183@gmail.com>
2022-11-16 21:05:50 +0300

    todo list app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+import Create from './components/create.js'
+import Read from './components/read'; 
+import Update from './components/update';  
+import {
+  createBrowserRouter,
+  RouterProvider,  
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",    
+    element: <Create />,
+  },
+  {
+    path: "/read",
+    element: <Read />,
+  },
+  {
+    path: "/update",
+    element: <Update />,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (    
+    <div className="main">
+      <h2 className="main-header"> React Crud Operations</h2>    
+      <RouterProvider router={router} />
+    </div>  
   );
 }
-
 export default App;
